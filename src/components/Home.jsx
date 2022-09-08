11d84552aea1c3b92e0b83c3ee552d64cf7bf647
AlbertoLanchas <albertolsn7@gmail.com>
2022-09-09 01:30:22 +0200

    database create
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
-import db from "../firebaseOld";
+import db from "../firebase";
 import NewDisney from "./NewDiseney";
 import Originals from "./Originals";
 import Trending from "./Trending";
+import { setMovies } from "../features/movies/moviesSlice";
+import { selectUserName } from "../features/user/userSlice";
+import {
+  collection,
+  getDoc,
+  getDocs,
+  doc,
+  onSnapshot,
+  getFirestore,
+} from "firebase/firestore";
 
 const Home = () => {
-  useEffect(() => {}, []);
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let newDisney = [];
+  let recommends = [];
+  let originals = [];
+  let trending = [];
+
+  const db = getFirestore();
+  const movieRef = collection(db, "movies");
+  getDoc(movieRef)
+    .then((snapshot) => {
+      let movies = [];
+      snapshot.docs.forEach((doc) => {
+        movies.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(movies);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 
   return (
     <Container>
